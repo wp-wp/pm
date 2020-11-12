791d7c966e5cd1dfd206294e35df396a5ecff26f
wp-wp <59838371+wp-wp@users.noreply.github.com>
2020-11-12 16:30:49 +0100

    Create kod_tabele.r

---
+++ kod_tabele.r
@@ -0,0 +1,55 @@
+library(rtape)
+library(parsedate)
+library(data.table)
+setwd("C:/Users/spicy/Desktop/Magisterka/")
+filenames <- list.files("airly", full.names=TRUE)
+
+nullGoesNA<-function(x) if(is.null(x)) NA else x
+
+getValue<-function(history,key){
+  for(x in history$values) if(identical(x$name,key)) return(x$value)
+  NA
+}
+
+getMeasurements<-function(history,key){
+  if(is.null(history[[1]]$values)){
+    #Old
+    sapply(history,function(x) nullGoesNA(x[[c('measurements',tolower(key))]]))
+  }else{
+    #New
+    sapply(history,getValue,toupper(key))
+  }
+}
+
+flattenAirlyRecord<-function(x){
+  if(is.null(x$ans)) return(NULL)
+  try(data.frame(
+    id=x$sensor$id,
+    lat=x$sensor$location$latitude,
+    lon=x$sensor$location$longitude,
+    time=parse_iso_8601(sapply(x$ans$history,'[[','fromDateTime')),
+    pm25=getMeasurements(x$ans$history,'pm25'),
+    pm10=getMeasurements(x$ans$history,'pm10')
+  ))
+}
+
+
+rtapeLapply(filenames[1],flattenAirlyRecord)->x  
+
+#epyc
+library(parallel)
+mclapply(rtapeAsList(filenames[1]),flattenAirlyRecord,mc.cores=96)->x
+
+#wyłapuje puste elementy (NULL)
+sapply(x,function(x) is.null(x)||inherits(x,"try-error"))->bad
+       
+library(data.table)
+#składa całość po wierszach
+rbindlist(x[!bad])->x
+       
+#pomija wiersze z minimum 1 NA
+na.omit(x)->x
+       
+#usuwa zduplikowane wiersze
+unique(x)->x
+
