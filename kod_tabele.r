df5f0594bba3c5cabb13a861fd0757443abfe737
wp-wp <wp.piskorz@gmail.com>
2021-01-04 10:10:34 +0100

    pull github desktop
    bez zmian

--- kod_tabele.r
+++ kod_tabele.r
@@ -46,7 +46,7 @@ loadAndRead<-function(file){
 
             ##epyc##
 loadAndReadParallel<-function(file){
-  parLapply(rtapeAsList(file),flattenAirlyRecord,mc.cores=48)->x
+  mclapply(rtapeAsList(file),flattenAirlyRecord,mc.cores=48)->x
   slapply(x,function(x) is.null(x)||inherits(x,"try-error"))->bad
   rbindlist(x[!bad])->x
   na.omit(x)->x
@@ -63,3 +63,34 @@ for(input_fn in list.files(pattern='tape$',full=TRUE)){
   }
 }
 
+##WCZYTYWANIE2##
+ptm <- proc.time()
+for(input_fn in list.files(pattern='tape$',full=TRUE)){
+  output_fn<-sprintf("%s.flattened.RDS",input_fn)
+  if(!file.exists(output_fn)){
+    saveRDS(loadAndReadParallel(input_fn),file=output_fn)         ##<--- bez pliku output##
+  }
+}
+proc.time()-ptm
+
+          
+          ##inne próby użyte z pętlą for##   ##obie wersje mają podobne czasy do zwykłej pętli for - funkcja loop jest minimalnie szybsza##
+loop<- function(x){                                       
+  output_fn<-sprintf("%s.flattened.RDS",x)
+  if(!file.exists(output_fn)){                            ##<--- zastanawialam sie czy nie zrobic funkcji w tym rodzaju i nie uzyc lapply, ale nie jestem pewna czy lapply nie 
+    saveRDS(loadAndReadParallel(input_fn),file=output_fn)         ##     zepsuloby plikow wynikowych##
+  }
+}
+
+##lub Windows##          
+oneFileLoop <-  function(x) {
+  output<- sprintf("%s.21flattened.RDS",x)
+  if(!file.exists(output_fn)){
+    rtapeLapply(x,flattenAirlyRecord) -> x
+    sapply(x,function(x) is.null(x)||inherits(x,"try-error"))->bad
+    rbindlist(x[!bad])->x 
+    na.omit(x)->x
+    unique(x)->x
+    saveRDS(x,file=output)
+    }
+}
