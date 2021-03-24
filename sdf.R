8600ea9dff92e130797ecb6ea5f54c821277050e
wp-wp <59838371+wp-wp@users.noreply.github.com>
2021-03-24 11:36:49 +0100

    bez pętli zew.
    - skrócony kod
    +do dodania pętla zew.

--- sdf.R
+++ sdf.R
@@ -1,5 +1,4 @@
 library(raster)
-library(sf)
 library(rgdal)
 
 ##wczytanie danych
@@ -8,21 +7,17 @@ points <- readOGR("KR_data.shp")
 setwd("")
 readOGR("primary.shp")->lines
 
-##sp to sf
-points_sf<- st_as_sf(points)
-lines_sf<- st_as_sf(lines)
-
-points_sf = st_transform(points_sf, st_crs(lines_sf)) # check crs
-grid <- st_make_grid(points_sf, cellsize = 300, what = "centers") ##create grid
-ext <- extent(as(grid, "Spatial")) ##get extent
-
 #create raster
-raster <- raster(resolution = 200, ext = ext, crs = "+init=epsg:2180 +proj=longlat +units=m +no_defs")
+raster <- raster(resolution = 200, ext = extent(points), crs = "+init=epsg:2180 +proj=longlat +units=m +no_defs")
 
 require(rgeos)
-raster_points <- as(r,"SpatialPoints") ##
+raster_points = as(raster,"SpatialPoints")
 distance_m <- gDistance(lines, raster_points, byid=TRUE) ##distance matrix
-raster[] = apply(dd,1,min) ##fill raster w/ min value from each row
+sdf<- apply(distance_m,1,min)
+raster[] = sdf  ##or raster[]=apply(distance_m,1,min)
 names(raster) <- 'distance'
+
+
+
 
 
