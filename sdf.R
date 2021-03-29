459b79cba95aa3bce6b65cd52b829f39c752fcef
wp-wp <59838371+wp-wp@users.noreply.github.com>
2021-03-29 11:25:23 +0200

    td:out

--- sdf.R
+++ sdf.R
@@ -1,22 +1,32 @@
 library(raster)
 library(rgdal)
+require(rgeos)
 
 ##wczytanie danych
 setwd("")
 points <- readOGR("KR_data.shp") 
 setwd("")
+##cr shp 4 primary/secondary/triary/etc
 readOGR("primary.shp")->lines
 
 #create raster
 raster <- raster(resolution = 200, ext = extent(points), crs = "+init=epsg:2180 +proj=longlat +units=m +no_defs")
-
-require(rgeos)
 raster_points = as(raster,"SpatialPoints")
 distance_m <- gDistance(lines, raster_points, byid=TRUE) ##distance matrix
 sdf<- apply(distance_m,1,min)
 raster[] = sdf  ##or raster[]=apply(distance_m,1,min)
 names(raster) <- 'distance'
 
+dToStreet<- function(points,lines){
+  raster <- raster(resolution = 200, ext = extent(points), crs = "+init=epsg:2180 +proj=longlat +units=m +no_defs")
+  raster_points = as(raster,"SpatialPoints")
+  distance_m <- gDistance(lines, raster_points, byid=TRUE) ##distance matrix
+  sdf<- apply(distance_m,1,min)
+  raster[] = sdf  ##or raster[]=apply(distance_m,1,min)
+  names(raster) <- 'distance'
+  
+  return(raster)
+  }
 
 
 
