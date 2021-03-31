9ab16d290bf811b5d9dbe3633f95e680300cbcba
wp-wp <wp.piskorz@gmail.com>
2021-03-31 15:17:15 +0200

    linux f setup, dodane pliki z danymi ulic, usuniete stare kody, poprawienie sdf.r

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# pm
+####
+pliki z ulicami (pojedyńcze wartstwy) w folderze pliki
+
+####
+WAW/KR foldery miast
+street_*.shp = ulice:
+primary
+primary_link
+secondary
+secondary_link
+tertiary
+tertiary_link
+
+####
+
--- sdf.R
+++ sdf.R
@@ -1,25 +1,25 @@
 library(raster)
 library(rgdal)
-require(rgeos)
+library(rgeos)
 
 ##wczytanie danych
 setwd("")
 points <- readOGR("KR_data.shp") 
-##cr shp 4 primary/secondary/triary/etc
-readOGR("primary.shp")->lines
+lines <- readOGR("street_KR.shp")
 
 ##CRS!!!
 points <- spTransform(points, CRS("+init=epsg:2180 +proj=longlat +units=m +no_defs"))
 lines <- spTransform(lines, CRS("+init=epsg:2180 +proj=longlat +units=m +no_defs"))
 
 
-#create raster
+#raster
 raster <- raster(resolution = 200, ext = extent(points), crs = "+init=epsg:2180 +proj=longlat +units=m +no_defs")
 raster_points = as(raster,"SpatialPoints")
 distance_m <- gDistance(lines, raster_points, byid=TRUE) ##distance matrix
 sdf<- apply(distance_m,1,min)
 raster[] = sdf  ##or raster[]=apply(distance_m,1,min)
 names(raster) <- 'distance'
+
 
 dToStreet<- function(points,lines){
   raster <- raster(resolution = 200, ext = extent(points), crs = "+init=epsg:2180 +proj=longlat +units=m +no_defs")
@@ -32,6 +32,5 @@ dToStreet<- function(points,lines){
   return(raster)
   }
 
-
-save(raster, file"r.RData")
+save(raster, file="r.RData")
 
