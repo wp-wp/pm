54810bb356d11c757c161d65b512692f441f20da
wp-wp <59838371+wp-wp@users.noreply.github.com>
2021-04-13 12:27:25 +0200

    up windows

--- kriging.R
+++ kriging.R
@@ -26,8 +26,9 @@ fKrig <- function(city){
   
 }
 
+##test na primary
 sdf<- function(city, pmloc){
-  points <- pmloc
+  points = pmloc
   coordinates(points) <- 3:4
   points <- spTransform(points, crs=("+proj=tmerc +lat_0=0 +lon_0=19 +k=0.9993 +x_0=500000 +y_0=-5300000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m
 +no_defs"))
