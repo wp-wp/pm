2bc50db0a6bec87ef507443ccf414674ef06cea9
wp-wp <wp.piskorz@gmail.com>
2021-04-14 19:10:51 +0200

    poprawione X,Y; do poprawy raster nrow error; zmiana zapisu

--- kriging.R
+++ kriging.R
@@ -3,39 +3,22 @@ library(fields)
 library(raster)
 library(rgeos)
 
+##################################
+###            DANE            ###
+##################################
 
-city_list <- list.dirs(recursive = F, full.names = F) ## folder=city
-data_dist <- list.files("raster",pattern= "*_primary.grd", recursive = T, full.names = T) ## raster Z
-data_pmloc <- list.files(pattern = "*_pmloc.RDS", recursive = T) ## smog Y
+city_list <- list.dirs(recursive = F, full.names = F)                                       ## LISTA FOLDEROW, FOLDER = MIASTO 
+data_dist <- list.files("raster",pattern= "*_primary.grd", recursive = T, full.names = T)   ## LISTA RASTROW (.GRD)
+data_pmloc <- list.files(pattern = "*_pmloc.RDS", recursive = T)                            ## DANE PM + LOKALIZACJA (.RDS)
+## street_level <- LISTA PLIKOW .SHP Z ULICAMI
 
+#####################################
+###            FUNKCJE            ###
+#####################################
 
-WAW_raster <- raster(data_dist[2]) 
-WAW_pmloc <- readRDS(data_pmloc[2])
-
-time <- unique(WAW_pmloc$m_t)
-time<- sort(time, decreasing = F)
-pmloc <- WAW_pmloc[WAW_pmloc$m_t %in% time[1],]
-
-pm = pmloc$pm10
-loc = data.frame(lon=pmloc$lon, lat=pmloc$lat)
-
-raster <- sdf(city_list[3], pmloc)
-  
-smog <- Krig()
-
-
-fKrig <- function(city){
-  
-}
-
-##test na primary
-sdf<- function(city, pmloc){
-  points = pmloc
-  coordinates(points) <- 3:4
-  proj4string(points) <- crs("+proj=longlat")
-  points <- spTransform(points, CRS="+proj=tmerc +lat_0=0 +lon_0=19 +k=0.9993 +x_0=500000 +y_0=-5300000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m
-+no_defs")
-  lines <- readOGR(paste0(city,"/ulice_", city,"/primary.shp"))
+sdf<- function(city){                                                                       ## FUNKCJA (MIASTO, ULICA)
+  points <- readOGR(paste0(city,"/pliki/", city,"_data.shp"))                               ## DO STWORZENIA .SHP ULIC (LISTA steet_level)
+  lines <- readOGR(paste0(city,"/ulice_", city,"/primary.shp"))                             ## LUB OBLICZENIE RASTROW OSOBNO
   lines <- spTransform(lines, crs(points))
   raster <- raster(resolution = 200, ext = extent(WAW_raster), crs = crs(points))
   raster_points = as(raster,"SpatialPoints")
@@ -45,4 +28,38 @@ sdf<- function(city, pmloc){
   names(raster) <- 'distance'
   return(raster)
 }
+
+fKrig <- function(city,raster){
+  
+  
+}
+
+########################################
+###            OBLICZENIA            ### TEST DLA WARSZAWY PRIMARY ROADS
+########################################
+
+stacja <- readRDS(data_pmloc[2])                          ## DANE
+Z = raster(data_dist[2])                                  ## ODLEGLOSCI OD ULICY
+
+t = unique(stacja$m_t)                                    ## CZAS
+t <- sort(t, decreasing = F)  
+
+### DLA KONKRETNEJ DATY ###
+
+XY <- stacja[stacja$m_t %in% t[5],]
+coordinates(XY) <- 5:6
+crs(XY) <- crs(Z)
+
+r <- raster(extent(Z), crs=crs(Z))
+
+X = cbind(XY$lon,XY$lat)                                  ## WSPOLRZEDNE STACJI
+Y10 = matrix(XY$pm10)                                     ## POZIOM PM10 (error qr.q2ty w/o matrix())
+Y25 = matrix(XY$pm25)                                     ## POZIOM PM25
+
+smog10 <- Krig(X,Y10, Z=)
+smog25 <- Krig()
+
+#########################################
+
+
 
