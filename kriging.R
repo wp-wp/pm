cb07e725256cc1dbff22f41741d3958f5c5d65b4
wp-wp <wp.piskorz@gmail.com>
2021-04-26 19:27:58 +0200

    funkcja kirg10(t,z) zalezna od czasu, dodanie bibliotek

--- kriging.R
+++ kriging.R
@@ -117,3 +117,72 @@ for(city in city_list){
 }
 surface(smog10)
 surface(smog10_list[[1]])
+
+##funkcje-test
+
+getZ <- function(city,street){
+  points <- readOGR(paste0(city,"/pliki/", city,"_data.shp"))
+  r <- raster(paste0("raster/", city,"_",street,".grd")) 
+  dist <- extract(r,points) #get min dist
+  r.pop = raster("GEOSTAT.tif")
+  r.proj = projectRaster(r.pop,r)
+  pop <- extract(r.proj, points) #get population
+  z=cbind(Z1=dist, Z2=pop)
+  
+  return(z)
+}
+
+city = "WAW"
+street = "primary.shp"
+
+getDF <- function(stacja){
+  stacja.loc <- data.frame(                                 ## df
+    m_id=stacja$m_id,
+    lon= stacja$lon,
+    lat=stacja$lat
+  )
+  stacja.loc <- unique(stacja.loc)
+  return(stacja.loc)
+}
+
+getT <- function(stacja){
+  t.all = unique(stacja$m_t)                                   
+  t.all <- sort(t.all, decreasing = F)
+  return(t.all)
+}
+
+krig10 <- function(t,Z){
+  stacja.dist <- cbind(stacja.loc,Z1=Z[,1], Z2= Z[,2])
+  xy <- stacja[stacja$m_t %in% t,]
+  xy <- merge(xy, stacja.dist[ , c("m_id", "Z1","Z2")], by="m_id")
+  if (nrow(xy)>6){
+    x = cbind(xy$lon,xy$lat)                                  
+    y10 = matrix(xy$pm10)                                     
+    z = cbind(xy$Z1, xy$Z2)
+    z[is.na(z)] <- mean(xy$Z2,na.rm=TRUE)                 ##populacja ma 2 wartości NA - zapełniam je średnią
+    try(smog10 <- Krig(x,y10,Z=z,theta = 50)) 
+      if (!inherits(smog10, "try-error")){
+        result<-smog10
+        } else {
+        result<-"Regression matrix for fixed part of model is colinear"
+        }
+     }else{
+        result<-"liczba pomiarów <7"
+    }
+  return(result)
+  }
+
+
+
+
+for (city in city_list) {
+  if (dir.exists(file.path(city, "pliki"))){ #warunek foldery
+    stacja <- readRDS(paste0(city[1],"/",city[1],"_pmloc.RDS"))  #wczytanie danych sensorow
+    stacja.loc <- getDF(stacja) #df zawierajaca id/lon/lat (w razie potrzeby zmienic lon/lat na e/n z innego pliku)
+    t = getT(stacja)  #wektor z wartościami czasu
+    for (street in street_list) {
+      Z = getZ(city[1],street[1])
+      krig_res<-krig10(t[14],Z)
+  }
+  }
+}
